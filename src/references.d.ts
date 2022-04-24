2693d9d25808adb50abaf47c84f6d473d5017f20
Nathan Walker <walkerrunpdx@gmail.com>
2022-04-24 10:04:21 -0700

    fix: SDK updates and build config fixes

--- src/references.d.ts
+++ src/references.d.ts
@@ -1,3 +1,3 @@
 /// <reference path="./node_modules/@nativescript/types-ios/index.d.ts" />
-/// <reference path="./node_modules/@nativescript/types-android/index.d.ts" />
+/// <reference path="./node_modules/@nativescript/types-android/lib/android-29.d.ts" />
 /// <reference path="./typings/objc!OMGHTTPURLRQ.d.ts" />
